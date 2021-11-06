b23e0456c94eb6e19f66461629b92a510902ad84
teslaXeno <paritosh.kumar@xeno.in>
2021-11-07 00:02:33 +0530

    updated tawk.to script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,9 +5,9 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
       type="text/javascript"
       dangerouslySetInnerHTML={{
         __html: `
-            let Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
             (function(){
-            const s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
             s1.src='https://embed.tawk.to/6186c1ef6885f60a50baa12e/1fjr6b57k';
             s1.charset='UTF-8';
